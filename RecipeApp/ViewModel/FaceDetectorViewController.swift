4c0cd390ad782c87d82abe18ca4866f8f4a38b9d
Sarah Sukamto <sarah.sukamto1998@gmail.com>
2019-10-14 16:16:37 +1100

    upon change, the face detection works, but code is still redundant

--- RecipeApp/ViewModel/FaceDetectorViewController.swift
+++ RecipeApp/ViewModel/FaceDetectorViewController.swift
@@ -13,7 +13,7 @@ class FaceDetectorViewController: UIViewController, UINavigationControllerDelega
 
     var faceDetected = false
     var imagePicker = UIImagePickerController()
-    var image: UIImage?
+    //var image: UIImage?
     var scaledHeight: CGFloat = 0
     var redBox = UIView()
     
@@ -57,10 +57,57 @@ class FaceDetectorViewController: UIViewController, UINavigationControllerDelega
 
     //Upon choosing an image, the photo on the screen will change to the selected image
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let chosenImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        imageView.image = chosenImage
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageView.image = image
         self.redBox.isHidden = true
-        detectFace()
+        self.faceDetected = false
+        
+        //Get the height of the image
+        scaledHeight = view.frame.width / image.size.width * image.size.height
+        
+        //Create a request to detect faces in the photo
+        let request = VNDetectFaceRectanglesRequest { (req, err) in
+            
+            if let err = err {
+                self.redBox.isHidden = true
+                print("Failed to detect faces: \(err)")
+                return
+            }
+            
+            req.results?.forEach({ (res) in
+                
+                guard let faceObservation = res as? VNFaceObservation else {return}
+                
+                //Set the box coordinates and size
+                let x = self.view.frame.width * faceObservation.boundingBox.origin.x
+                let height = self.scaledHeight * faceObservation.boundingBox.height
+                let y = self.view.frame.height * (1-faceObservation.boundingBox.origin.y)-height
+                let width = self.view.frame.width * faceObservation.boundingBox.width
+                
+                //Create the box
+                self.redBox.isHidden = false
+                self.redBox.backgroundColor = .red
+                self.redBox.alpha = 0.4
+                self.redBox.frame = CGRect(x: x, y: y, width: width, height: height)
+                
+                self.view.addSubview(self.redBox)
+                
+                print(faceObservation.boundingBox)
+                
+                self.faceDetected = true
+            })
+        }
+        
+        guard let cgImage = image.cgImage else {return}
+        
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        
+        do{
+            try handler.perform([request])
+        } catch let errReq{
+            print("Failed to perform request: \(errReq)")
+        }
+        
         dismiss(animated:true, completion: nil)
     }
     
@@ -123,8 +170,6 @@ class FaceDetectorViewController: UIViewController, UINavigationControllerDelega
     }
 
     func detectFace(){
-        
-        
         
     }
 }
